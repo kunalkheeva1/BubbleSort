af838996883e97fc65ffe74ffc3ec39d455334bf
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-06-09 02:02:40 -0700

    now i have added another reverse print method to print bubble sort in reverse method as my actual algorithm sorts the array in reverse order

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -1,14 +1,21 @@
 
 //unintentionally i created an algorithm which is descending bubble sort
 public class BubbleSort {
+
     public static void bubbleSort(int []arr){
+        boolean isSwapped;                      //creating a boolean variable to keep the track of the swaps
         for(int i=0; i<arr.length-1; i++){
+            isSwapped = false;                  //initially i am assuming that there will be no swap
             for (int j=0; j<arr.length-1; j++){
-                if (arr[j + 1] > arr[j]){
+                if (arr[j] > arr[j+1]){
                     int temp = arr[j];
                     arr[j] =arr[j+1];
                     arr[j+1]=temp;
+                    isSwapped = true;               // but here the swap is true if the above if conditions apply
                 }
+            }//now if the is swapped is false then it will break the loop
+            if(isSwapped=false){
+                break;
             }
         }
     }
@@ -30,7 +37,7 @@ public class BubbleSort {
     //testing the methods in the actual loops
 
     public static void main(String[] args) {
-        int []arr= {2,35,1,3,5,5,4};
+        int []arr= {2,35,1,3,5,5,4, 2,3,1,9,10,32,92};
         print(arr);
         bubbleSort(arr);
         print(arr);
